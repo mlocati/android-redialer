e1165bae7129ae83aaac5c780ffc4056632e9c51
Michele Locati <michele@locati.it>
2026-04-01 11:34:15 +0200

    Version 1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "it.locati.michele.redialer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.2.0"
+        versionCode = 3
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
