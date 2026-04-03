cf447e31c433df73b89c5f0ab42809817103849e
Michele Locati <michele@locati.it>
2026-04-03 13:34:35 +0200

    Version 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "it.locati.michele.redialer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.3.1"
+        versionCode = 14
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
