7dab0343b3c888d80066a5b5087aeb556d7e4876
Michele Locati <michele@locati.it>
2026-04-03 12:30:37 +0200

    [minor] Enable Develocity build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,7 @@ pluginManagement {
     }
 }
 plugins {
+    id("com.gradle.develocity") version "4.4.0"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 dependencyResolutionManagement {
@@ -19,6 +20,13 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+    }
+}
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+        termsOfUseAgree = "yes"
+        publishing.onlyIf { !System.getenv("CI").isNullOrEmpty() }
     }
 }
 
