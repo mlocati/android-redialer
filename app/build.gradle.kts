c2169dcb9ff9fd52a3c9a3af94b6554459b67c74
GitHub <noreply@github.com>
2026-04-07 13:28:02 +0000

    Version 1.5.0-alpha.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "it.locati.michele.redialer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.5.0-alpha.2"
+        versionCode = 19
+        versionName = "1.5.0-alpha.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
